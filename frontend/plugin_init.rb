9e0b9bde976abfa7db54c4df4c2287aebd8661ac
Payten Giles <payten.giles@gmail.com>
2015-04-29 09:52:32 +1000

    Extend the base facets to include 'level' to allow faceting of advanced/text search results by a component's level

--- frontend/plugin_init.rb
+++ frontend/plugin_init.rb
@@ -27,4 +27,12 @@ Rails.application.config.after_initialize do
     end
   end
 
+
+  SearchResultData.class_eval do
+    self.singleton_class.send(:alias_method, :BASE_FACETS_pre_component_report, :BASE_FACETS)
+    def self.BASE_FACETS
+      self.BASE_FACETS_pre_component_report << "level"
+    end
+  end
+
 end
