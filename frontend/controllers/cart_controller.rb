ab2d6d436887408906592b58c2f794a094f98991
Payten Giles <payten.giles@gmail.com>
2015-04-28 21:41:12 +1000

    WIP use a tmp file so we use Rails' inbuild file streaming

--- frontend/controllers/cart_controller.rb
+++ frontend/controllers/cart_controller.rb
@@ -1,3 +1,6 @@
+require 'tempfile'
+require 'securerandom'
+
 class CartController < ApplicationController
 
   set_access_control  "view_repository" => [:checkout, :summary, :download_report]
@@ -22,73 +25,89 @@ class CartController < ApplicationController
   def download_report
     uris = ASUtils.as_array(params[:uri])
 
-    queue = Queue.new
+    tmp_file = Tempfile.new("component_report_#{SecureRandom.hex}.xlsx")
 
-    backend_session = JSONModel::HTTP::current_backend_session
+    report_response = JSONModel::HTTP.post_form("/plugins/component_report/repositories/#{session[:repo_id]}/report", "uri[]" => uris)
+    tmp_file.write(report_response.body)
 
-    Thread.new do
-      JSONModel::HTTP::current_backend_session = backend_session
-      begin
-        post_with_stream_response("/plugins/component_report/repositories/#{session[:repo_id]}/report", "uri[]" => uris) do |report_response|
-          response.headers['Content-Disposition'] = report_response['Content-Disposition']
-          response.headers['Content-Type'] = report_response['Content-Type']
-          #response.headers['Content-Type'] = "text/html"
-          response.headers['Last-Modified'] = Time.now.to_s
-          response.headers['Cache-Control'] = 'no-cache'
-          response.headers['X-Content-Type-Options'] = 'nosniff'
-          #response.headers['Transfer-Encoding'] = 'chunked'
+    send_file tmp_file, :type => "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", :disposition => "attachment", :filename => "component_report.xlsx"
 
-          queue << :ok
-          report_response.read_body do |chunk|
-            puts ""
-            p "++ PRODUCED: #{chunk}"
-            queue << chunk
-          end
-        end
-      rescue EOFError
-        puts "!! RESCUE EOFError"
-        p $!
-        queue << :EOF
-      rescue
-        puts "!! RESCUE"
-        p $!
-        queue << {:error => $!.message}
-      ensure
-        puts ""
-        p "++ PRODUCED: EOF"
-        queue << :EOF
-      end
-    end
 
-    first_on_queue = queue.pop # :ok or error hash
-    if first_on_queue.kind_of?(Hash)
-      @report_errors = first_on_queue[:error]
+    # queue = Queue.new
+    #
+    # backend_session = JSONModel::HTTP::current_backend_session
+    #
+    # Thread.new do
+    #   JSONModel::HTTP::current_backend_session = backend_session
+    #   begin
+    #     post_with_stream_response("/plugins/component_report/repositories/#{session[:repo_id]}/report", "uri[]" => uris) do |report_response|
+    #       response.headers['Content-Disposition'] = report_response['Content-Disposition']
+    #       #response.headers['Content-Disposition'] = "inline; filename=\"component_report.xlsx\""
+    #       response.headers['Content-Type'] = report_response['Content-Type']
+    #       #response.headers['Content-Type'] = "text/html"
+    #       response.headers['Last-Modified'] = Time.now.to_s
+    #       response.headers['Cache-Control'] = 'no-cache'
+    #       response.headers['X-Content-Type-Options'] = 'nosniff'
+    #       #response.headers['Transfer-Encoding'] = 'chunked'
+    #
+    #       queue << :ok
+    #       report_response.read_body do |chunk|
+    #         puts ""
+    #         p "++ PRODUCED: #{chunk}"
+    #         queue << chunk
+    #       end
+    #     end
+    #   rescue EOFError
+    #     puts "!! RESCUE EOFError"
+    #     p $!
+    #     queue << :EOF
+    #   rescue
+    #     puts "!! RESCUE"
+    #     p $!
+    #     queue << {:error => $!.message}
+    #   ensure
+    #     puts ""
+    #     p "++ PRODUCED: EOF"
+    #     queue << :EOF
+    #   end
+    # end
+    #
+    # first_on_queue = queue.pop # :ok or error hash
+    # if first_on_queue.kind_of?(Hash)
+    #   @report_errors = first_on_queue[:error]
+    #
+    #   return render :action => :checkout
+    # end
 
-      return render :action => :checkout
-    end
+    #self.response_body = QueueStreamer2.new(queue)
 
-    #self.response_body = QueueStreamer.new(queue)
-
-    self.response_body = Class.new do
-      def self.queue=(queue)
-        @queue = queue
-      end
-      def self.each(&block)
-        while(true)
-          chunk = @queue.pop
-
-          puts ""
-          p "-- CONSUMED: #{chunk}"
-
-          break if chunk === :EOF
-
-          block.call(chunk)
-          sleep(0.2)
-        end
-      end
-    end
-
-    self.response_body.queue = queue
+    # self.response_body = Class.new do
+    #   def self.queue=(queue)
+    #     @queue = queue
+    #   end
+    #   def self.each(&block)
+    #     while(true)
+    #       chunk = @queue.pop
+    #
+    #       puts ""
+    #       p "-- CONSUMED: #{chunk}"
+    #
+    #       if chunk === :EOF
+    #         p "-- BREAKING EOF"
+    #         break
+    #       end
+    #
+    #       #send_me = [chunk.bytesize.to_s(16), "\r\n", chunk, "\r\n"].join
+    #       #send_me = [chunk.bytesize.to_s(16), "\r\n", chunk, "\r\n"].join
+    #       #p "-- SEND ME: #{send_me}"
+    #
+    #       block.call(chunk)
+    #       #sleep(0.1)
+    #     end
+    #   end
+    # end
+    #
+    # self.response_body.queue = queue
   end
 
 
@@ -131,11 +150,37 @@ class QueueStreamer
       puts ""
       p "-- CONSUMED: #{chunk}"
 
-      break if chunk === :EOF
+      if chunk === :EOF
+        p "-- BREAKING EOF"
+        break
+      end
 
       block.call(chunk)
-      sleep(0.1)
+      #sleep(0.1)
     end
+  end
+end
+
+
+class QueueStreamer2
+
+  def initialize(queue)
+    @queue = queue
+  end
+
+  def each
+    while(true)
+      chunk = @queue.pop
+
+      puts ""
+      p "-- CONSUMED: #{chunk}"
+
+      break if chunk === :EOF
+
+      yield chunk
+      #sleep(0.1)
+    end
+    p "---- !!! OUTSIDE WHILE LOOP"
   end
 
 end
